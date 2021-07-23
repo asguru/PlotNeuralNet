69b99344d8a2f5aebc216a652523f76bf940ce5f
Arjun G <aguru@seas.upenn.edu>
2021-07-23 03:28:10 -0400

    fixed shapes+dims

--- pycore/tikzeng.py
+++ pycore/tikzeng.py
@@ -48,12 +48,12 @@ def to_Conv( name, s_filer=256, n_filer=64, offset="(0,0,0)", to="(0,0,0)", widt
         name=""" + name +""",
         caption="""+ caption +r""",
         xlabel={{"""+ str(n_filer) +""", }},
-        ylabel={{"""+ str(int(n_filer * (3.0/4.0))) +""", }},
+        ylabel={{"""+ str(int(s_filer * (3.0/4.0))) +""", }},
         zlabel="""+ str(s_filer) +""",
         fill=\ConvColor,
         height="""+ str(height) +""",
         width="""+ str(width) +""",
-        depth="""+ str(int(depth * (3.0 / 4.0))) +"""
+        depth="""+ str(depth) +"""
         }
     };
 """
